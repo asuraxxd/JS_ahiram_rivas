e87f6526918ad943d90d895ee330b943766eb7e9
asuraxxd <ahiram.rivas@institutvidreres.cat>
2024-11-13 18:54:28 +0100

    exercici 2

--- JS_ahiram_rivas.js
+++ JS_ahiram_rivas.js
@@ -5,3 +5,13 @@
 console.log("Ahiram")
 console.warn("Rivas")
 console.error("2005")
+
+//2.- Declara 3 variables, cadascuna amb un tipus de dades diferent i mostra el valor per consola.
+
+let nombre ="ahiram"
+let estudiant=true
+let edad= 19
+
+console.log(nombre)
+console.log(estudiant)
+console.log(edad)
