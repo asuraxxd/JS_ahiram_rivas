eabbe00ff8d3c0515e7318b5508a4636840be546
asuraxxd <ahiram.rivas@institutvidreres.cat>
2025-11-07 09:02:08 +0100

    ejercicio1

---
+++ main.js
@@ -0,0 +1,5 @@
+//1.- Mostra per consola el teu nom, cognoms i any de naixement utilitzant diferents missatges de consola.
+
+console.log("Ahiram")
+console.error("Rivas")
+console.warn("19/10/2005")
