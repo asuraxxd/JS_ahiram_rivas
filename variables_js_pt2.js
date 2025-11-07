c98d2421c9e03e7aad5fa222413052547ffbab56
asuraxxd <ahiram.rivas@institutvidreres.cat>
2025-11-07 11:38:45 +0100

    ejercicio 9

--- variables_js_pt2.js
+++ variables_js_pt2.js
@@ -3,30 +3,36 @@
 let num = 1234
 let bigint = Number(5678n)
 
-let suma= bigint+num
-let resta=bigint-num
-let multiplicacion=bigint*num
-let division=bigint/num
+let suma = bigint + num
+let resta = bigint - num
+let multiplicacion = bigint * num
+let division = bigint / num
 
-console.log('Suma: ' +suma.toFixed(1))
-console.log('Resta: ' +resta.toFixed(1))
-console.log('Multiplicacion: ' +multiplicacion.toFixed(1))
-console.log('Division: ' +division.toFixed(1))
+console.log('Suma: ' + suma.toFixed(1))
+console.log('Resta: ' + resta.toFixed(1))
+console.log('Multiplicacion: ' + multiplicacion.toFixed(1))
+console.log('Division: ' + division.toFixed(1))
 
 //7.- Calcula la hipotenusa d’un triangle on les longituds dels catets sigui de 4.
 
-let cateto1=4
-let cateto2=4
+let cateto1 = 4
+let cateto2 = 4
 
 
-let elevadoPotencia=(cateto1**2)+(cateto2**2)
-let hipotenusa=Math.sqrt(elevadoPotencia)
-console.log('El valor de la hipotenusa es: '+hipotenusa.toFixed(2))
+let elevadoPotencia = (cateto1 ** 2) + (cateto2 ** 2)
+let hipotenusa = Math.sqrt(elevadoPotencia)
+console.log('El valor de la hipotenusa es: ' + hipotenusa.toFixed(2))
 
 //8.- Divideix un enter entre un String. Què retorna?
 
-let numeroEntero=123
-let cadenaTexto="hola"
+let numeroEntero = 123
+let cadenaTexto = "hola"
 
-let division2=numeroEntero/cadenaTexto
+let division2 = numeroEntero / cadenaTexto
 console.log(division2)
+
+//9.- Donat un String amb el teu nom i cognoms, extreu el teu primer cognom, i mostra’l per pantalla indicant el número de caràcters que té.
+
+let nombreApellidos="Ahiram Rivas Illanes"
+let apellido1=nombreApellidos.slice(7,12)
+console.log(apellido1+' El numero de caracteres del pimer apellido es: '+ apellido1.length)
